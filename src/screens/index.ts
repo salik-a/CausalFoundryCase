9d795b1c9b181fba970fc9e99f070631445fdade
salik-a <salika@mef.edu.tr>
2024-10-28 02:12:07 +0300

    unused files removed

--- src/screens/index.ts
+++ src/screens/index.ts
@@ -1,11 +1,8 @@
-import CounterScreen from "./CounterScreen"
-import MenuScreen from "./MenuScreen"
-import WelcomeScreen from "./WelcomeScreen"
 import LoginScreen from "./LoginScreen"
 import PostsScreen from "./PostsScreen"
 import PostsScreenDetail from "./PostsScreenDetail"
 
 export * from "./ErrorScreen/ErrorBoundary"
 
-export { WelcomeScreen, MenuScreen, CounterScreen, LoginScreen, PostsScreen, PostsScreenDetail }
+export { LoginScreen, PostsScreen, PostsScreenDetail }
 // export other screens here
